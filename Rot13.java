33bf6e31e943fd5706c8bc19253386b87546bde9
Yago Garcia <yagog5002@gmail.com>
2024-09-19 15:40:03 +0200

    Siguiendo con el ex 1

--- Rot13.java
+++ Rot13.java
@@ -4,13 +4,15 @@
  *  posición 13 despues de esta.
  *  private void desxifraRot13(String cadena): descifra a la inversa que xifraRot13
  */
+import java.util.Scanner;
+
 public class Rot13{
-    private final char[] abecedarioMayus = {'A', 'B', 'C', 'Ç','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    private final char[] abecedarioMinus = {'a', 'b', 'c', 'ç','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    public static final char[] abecedarioMayus = {'A', 'B', 'C', 'Ç','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    public static final char[] abecedarioMinus = {'a', 'b', 'c', 'ç','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     //función para cifrar
-    private void xifraRot13(String cadena){
-        if(cadena.isBlank()){ return;} //si la cadena esta vacia no hace nada
+    public static String xifraRot13(String cadena){
+        if(cadena.isBlank()){ return "";} //si la cadena esta vacia no hace nada
 
         String cadenaCifrada = "";
         for(int letraCadenaINT = 0;letraCadenaINT<cadena.length();letraCadenaINT++){ //bucle que recorre la cadena que se introduce en el parametro
@@ -20,7 +22,32 @@ public class Rot13{
                 if(letraAbecedarioCHR!=letraCadenaCHR){ //si no es la misma letra, continua con la siguiente letra
                     continue;
                 }
+                boolean sePasa = letraAbecedarioINT + 13 >= abecedarioMayus.length; // boolean que indica si se pasa o no
+                int a = sePasa ? XXX : letraAbecedarioINT + 13; 
+                int b = XXX;
+                if(sePasa){
+                    cadenaCifrada += XXX;
+                    continue;
+                } 
+                cadenaCifrada += abecedarioMayus[a]; //position = i + 13
+                continue;
             }
         }
+        return cadenaCifrada;
+    }
+
+    public static void main(String[] args) {
+        String texto = "";
+        try(Scanner scanner = new Scanner(System.in)){
+            System.out.printf("Ingresa un texto:");
+            texto = scanner.nextLine();
+            System.out.println();
+        }
+        if(xifraRot13(texto).isBlank()){
+            System.out.println("El texto no puede ser en blanco");
+            return;
+        }
+        System.out.printf("Texto cifrado: %s%n",xifraRot13(texto));
+
     }
 }
